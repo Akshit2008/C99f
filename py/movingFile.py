00b6fbca5aa2781a8750d2438810a68fcc2053f7
Akshit2008 <85681429+Akshit2008@users.noreply.github.com>
2022-04-19 16:48:37 +0530

    Add files via upload

---
+++ py/movingFile.py
@@ -0,0 +1,5 @@
+import os
+import shutil
+source="C:/users/kulka/desktop/C99/destination/destination.txt"
+destination="C:/users/kulka/desktop/C99/source/source.txt"
+dest=shutil.move(source,destination)
